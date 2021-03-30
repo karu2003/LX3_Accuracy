796d1c7696d96e34e90dffddcf97f9553fecf337
karu2003 <ka_ru2003@msn.com>
2021-03-30 16:59:55 +0100

    Add BPM measuring.

--- LX3_accuracy.py
+++ LX3_accuracy.py
@@ -7,8 +7,11 @@ import numpy as np
 from datetime import datetime
 import csv
 import Adafruit_ADS1x15
+import sounddevice as sd
+from estimate_bpm import BPM_Analyzer
 
 adc = Adafruit_ADS1x15.ADS1115()
+bpms = BPM_Analyzer()
 
 ADC_Scale = 4.096/32768.0 
 
@@ -16,16 +19,13 @@ GAIN = 1
 biasVoltage = 1.5 
 
 def getAbsoluteAngle():
-    # sin = (adc.read_adc(0, gain=GAIN) * ADC_Scale) - biasVoltage
-    # cos = (adc.read_adc(1, gain=GAIN) * ADC_Scale) - biasVoltage
-
     # 0 = Channel 0 minus channel 1
     sin = adc.read_adc_difference(0, gain=GAIN) * ADC_Scale 
     # 3 = Channel 2 minus channel 3
     cos = adc.read_adc_difference(3, gain=GAIN) * ADC_Scale
     # angle = (np.arctan2(sin,cos)*(180/3.1415928353)) + 180
     # Radian
-    angle = np.arctan2(sin,cos) + 3.1415928353 #np.pi 
+    angle = np.arctan2(sin,cos) + 3.1415928353 
     return angle
 
 def date_now():
@@ -39,7 +39,7 @@ def time_now():
     return(now)
 
 # filename = 'LX3_'+(date_now())+'_'+(time_now())+'.csv'
-filename = 'LX3_'+(date_now())+'.csv'
+filename = 'LX3_BPM_'+(date_now())+'.csv'
 
 # Start_Time = time.time()
 Start_Time = time.monotonic()
@@ -49,12 +49,15 @@ Angle_old = 0
 Delta_Angle = 0
 Start_Angle = round(getAbsoluteAngle(),6)
 
-print('Angular Velocity')   
-print('{0:<23} {1:<23} {2:<24}'.format('Absolute Angle','instant','mean'))
+print('Angular Velocity & BPM')   
+print('{0:<23} {1:<23} {2:<24}'.format('instant','mean','BPM'))
 
 csv = open(filename, 'w')
-csv.write('AV instant,AV mean\n')
+csv.write('AV instant,AV mean,BPM\n')
 csv.close
+
+fs = 44100      # Sample rate
+seconds = 3.0    # Duration of recording
 
 #polling
 while True:
@@ -72,25 +75,21 @@ while True:
             loop_cnt = 0
             continue 
 
+        audio_input = sd.rec(int(seconds * fs), samplerate=fs, channels=1)
+        sd.wait()      
+        correlation, bpm = bpms.computeWindowBPM(audio_input.flatten(),fs)
+
         Angle_old = AbsoluteAngle
         Angular_Velocity_instant = Delta_Angle/loop_Time
         Angular_Velocity_mean = (AbsoluteAngle - Start_Angle)/(loop_cnt*loop_Time)
 
         if Angular_Velocity_mean:
-            print('{0:<23.7E} {1:<23.7E} {2:<24.7E}'.format(AbsoluteAngle, Angular_Velocity_instant, Angular_Velocity_mean))
-            entry = str(Angular_Velocity_instant) + "," + str(Angular_Velocity_mean) + "\n"
+            print('{0:<23.7E} {1:<23.7E} {2:<24}'.format(Angular_Velocity_instant, Angular_Velocity_mean,str(np.mean(bpm))))
+            entry = str(Angular_Velocity_instant) + "," + str(Angular_Velocity_mean) + "," + str(np.mean(bpm)) + "\n"
  
         csv = open(filename, 'a')
         csv.write(entry)
 
-        # try:
-        #     csv.write(entry)
-        # finally:
-        #     csv.close()
-
     except KeyboardInterrupt:
         csv.close()
-        # csv = open(filename, 'r')
-        # print(csv.read())
-        # csv.close()
         sys.exit('\nInterrupted by user')     
